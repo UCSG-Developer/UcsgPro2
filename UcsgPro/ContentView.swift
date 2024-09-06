83e6a4f91399bff607cd111e4cf005f3f93c3778
Eduardo M. Gómez A <eduardo.gomez@cu.ucsg.edu.ec>
2024-09-06 10:07:19 -0500

    Nueva página

--- UcsgPro/ContentView.swift
+++ UcsgPro/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                     Image(systemName: "globe")
                         .imageScale(.large)
                         .foregroundStyle(.tint)
-                    Text("Hello, world of Git!")
+                    Text("Hello, world of GitHUB!")
                 }
                 .padding()
             }
---
+++ UcsgPro/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  UcsgPro
+//
+//  Created by Eduardo Gomez on 6/9/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("Hello, Segunda Pantalla de prueba")
+    }
+}
+
+#Preview {
+    SecondView()
+}
