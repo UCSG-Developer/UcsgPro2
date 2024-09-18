e52b3e92ce729e4c2b5b10a8e1981f235e18688c
Eduardo M. Gómez A <eduardo.gomez@cu.ucsg.edu.ec>
2024-09-18 14:27:28 -0500

    Primera version completa

---
+++ UcsgPro/BibliotecasView.swift
@@ -0,0 +1,18 @@
+//
+//  BibliotecasView.swift
+//  UcsgPro
+//
+//  Created by Eduardo Gomez on 13/9/24.
+//
+
+import SwiftUI
+
+struct BibliotecasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BibliotecasView()
+}
---
+++ UcsgPro/MateriaView.swift
@@ -0,0 +1,18 @@
+//
+//  MateriaView.swift
+//  UcsgPro
+//
+//  Created by Eduardo Gomez on 13/9/24.
+//
+
+import SwiftUI
+
+struct MateriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MateriaView()
+}
